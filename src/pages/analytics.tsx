bd38e82818dc4755107239c54cd18bea39da9443
Victor Leung <victorleungtw@gmail.com>
2023-05-20 06:38:35 +0000

    Update analytics data

--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -6,18 +6,18 @@ import Layout from '@/components/Layout/Layout';
 const data = [
   {
     Month: 'Jan 21',
-    Sales: 2890,
-    Profit: 2400
+    Savings: 890,
+    NoSavings: 890
   },
   {
     Month: 'Feb 21',
-    Sales: 1890,
-    Profit: 1398
+    Savings: 1890,
+    NoSavings: 1398
   },
   {
     Month: 'Jan 22',
-    Sales: 3890,
-    Profit: 2980
+    Savings: 3890,
+    NoSavings: 2980
   }
 ];
 
@@ -29,12 +29,12 @@ export default function Analytics() {
     <Layout>
     <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] pt-10">
       <Card className="mt-8">
-        <Title>Performance</Title>
-        <Text>Comparison between Sales and Profit</Text>
+        <Title>10 Years Projection</Title>
+        <Text>Comparison between Savings and No Savings</Text>
         <AreaChart
           className="mt-4 h-80"
           data={data}
-          categories={['Sales', 'Profit']}
+          categories={['Savings', 'NoSavings']}
           index="Month"
           colors={['indigo', 'fuchsia']}
           valueFormatter={valueFormatter}
