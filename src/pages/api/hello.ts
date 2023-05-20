6a8eb2e6cac67c292bc6eaf44bc1997b40c9fd91
Marcus Pang <marcuspangyuyang@gmail.com>
2023-05-20 09:28:26 +0800

    feat: test mongodb with api routes

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from '@/lib/mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  isConnected: boolean;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'GET') {
+    await clientPromise;
+    return res.status(200).json({ isConnected: true });
+  }
+  return res.status(200).json({ isConnected: false });
 }
