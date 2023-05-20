318764bf39fea5df0634fc8170f1ff589d4fb2d6
Victor Leung <victorleungtw@gmail.com>
2023-05-20 04:30:27 +0000

    Add analytics chart

--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -1,3 +1,41 @@
-export default function Analytics() {
-    return <div>Analytics</div>;
+'use client';
+
+import { Card, AreaChart, Title, Text } from '@tremor/react';
+
+const data = [
+  {
+    Month: 'Jan 21',
+    Sales: 2890,
+    Profit: 2400
+  },
+  {
+    Month: 'Feb 21',
+    Sales: 1890,
+    Profit: 1398
+  },
+  {
+    Month: 'Jan 22',
+    Sales: 3890,
+    Profit: 2980
   }
+];
+
+const valueFormatter = (number: number) =>
+  `$ ${Intl.NumberFormat('us').format(number).toString()}`;
+
+export default function Analytics() {
+  return (
+    <Card className="mt-8">
+      <Title>Performance</Title>
+      <Text>Comparison between Sales and Profit</Text>
+      <AreaChart
+        className="mt-4 h-80"
+        data={data}
+        categories={['Sales', 'Profit']}
+        index="Month"
+        colors={['indigo', 'fuchsia']}
+        valueFormatter={valueFormatter}
+      />
+    </Card>
+  );
+}
