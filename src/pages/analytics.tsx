7cebac40159b4763197c1a971c75d519daaca7c8
Victor Leung <victorleungtw@gmail.com>
2023-05-20 06:30:25 +0000

    Update layout for analytics

--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Card, AreaChart, Title, Text } from '@tremor/react';
+import Layout from '@/components/Layout/Layout';
 
 const data = [
   {
@@ -25,17 +26,21 @@ const valueFormatter = (number: number) =>
 
 export default function Analytics() {
   return (
-    <Card className="mt-8">
-      <Title>Performance</Title>
-      <Text>Comparison between Sales and Profit</Text>
-      <AreaChart
-        className="mt-4 h-80"
-        data={data}
-        categories={['Sales', 'Profit']}
-        index="Month"
-        colors={['indigo', 'fuchsia']}
-        valueFormatter={valueFormatter}
-      />
-    </Card>
+    <Layout>
+    <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] pt-10">
+      <Card className="mt-8">
+        <Title>Performance</Title>
+        <Text>Comparison between Sales and Profit</Text>
+        <AreaChart
+          className="mt-4 h-80"
+          data={data}
+          categories={['Sales', 'Profit']}
+          index="Month"
+          colors={['indigo', 'fuchsia']}
+          valueFormatter={valueFormatter}
+        />
+      </Card>
+    </div>
+  </Layout>
   );
 }
